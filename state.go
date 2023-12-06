5616c79543aae6d3fd435300796fd8383a61074c
Danila Stivrinsh <danilaridzhi@gmail.com>
2023-12-06 17:34:38 +0300

    upd

--- state.go
+++ state.go
@@ -4,6 +4,18 @@ import (
 	"sort"
 )
 
+func StateFrom(m ...Mutator) (*State, error) {
+	s := new(State)
+
+	for _, v := range m {
+		if err := v.Mutate(s); err != nil {
+			return s, err
+		}
+	}
+
+	return s, nil
+}
+
 type State struct {
 	Age       age       `json:"age"`
 	Phase     phase     `json:"phase"`
